b767e222745a584309e2add14aa363369a1000f8
Dante Herrera <39971723+danteherrera1999@users.noreply.github.com>
2025-06-11 21:03:22 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+const svgNS = "http://www.w3.org/2000/svg";
+const lw = 5;
 
 class rightClickMenu {
   constructor(element) {
@@ -32,6 +33,46 @@ class rightClickMenu {
   }
 }
 
+class Edge{
+  constructor(p1,p2){
+    this.element = document.createElementNS(svgNS,"svg")
+    this.path = document.createElementNS(svgNS,"path")
+    this.element.appendChild(this.path);
+    this.element.classList.add("edge_svg")
+    this.path.classList.add("edge");
+    //M 0 0 Q 0 50, 30 50 Q 60 50, 60 100
+    this.getCurveParams(p1,p2);
+    this.buildCurve()
+  }
+  buildCurve(){
+    const w = this.width;
+    const h = this.height;
+    const d = (this.p_top[0]>this.p_bot[0])?(
+      `M 0 0 Q 0 ${lw+(h-lw)/2}, ${(w-lw)/2} ${lw+(h-lw)/2} Q ${w-lw} ${lw+(h-lw)/2}, ${w-lw} ${h}
+      H ${w} Q ${w} ${(h-lw)/2}, ${(w-lw)/2+lw} ${(h-lw)/2} Q ${lw} ${(h-lw)/2}, ${lw} ${0} z`
+    ):
+    (
+      `M ${w} 0 Q ${w} ${lw+(h-lw)/2}, ${lw+(w-lw)/2} ${lw+(h-lw)/2} Q ${lw} ${lw+(h-lw)/2}, ${lw} ${h}
+      H 0 Q 0 ${(h-lw)/2}, ${(w-lw)/2} ${(h-lw)/2} Q ${w-lw} ${(h-lw)/2}, ${w-lw} ${0} z`
+    )
+    this.path.setAttribute("d",d)
+  }
+  getCurveParams(p1,p2){
+    this.p_top= p1[1]>p2[1]? p1:p2;
+    this.p_bot= p1[1]<=p2[1]? p1:p2;
+    this.width = Math.abs(p2[0]-p1[0]);
+    this.height = Math.abs(p2[1]-p1[1]);
+    this.element.setAttribute("width",this.width);
+    this.element.setAttribute("height",this.height);
+    this.element.style.top = `${this.p_bot[1]}px`;
+    this.element.style.left = `${Math.min(p1[0],p2[0])}px`;
+  }
+  rebuildCurve(p1,p2){
+    this.getCurveParams(p1,p2);
+    this.buildCurve();
+  }
+}
+
 class Node {
   constructor(x, y, id) {
     this.id = id;
@@ -49,18 +90,18 @@ class Node {
     this.creatEboxes();
   }
   creatEboxes() {
-    const newTopEbox = document.createElement("div");
-    const newBotEbox = document.createElement("div");
-    newTopEbox.classList.add("nodeEbox");
-    newBotEbox.classList.add("nodeEbox");
-    newTopEbox.addEventListener("mousedown", (e) => { e.stopPropagation(); e.preventDefault(); this.handleEboxClick("top") })
-    newBotEbox.addEventListener("mousedown", (e) => { e.stopPropagation(); e.preventDefault(); this.handleEboxClick("bot") })
-    newTopEbox.addEventListener("mouseup", (e) => { this.checkNewEdge(e,"top") });
-    newBotEbox.addEventListener("mouseup", (e) => { this.checkNewEdge(e,"bot") });
-    newTopEbox.style.top = 0;
-    newBotEbox.style.bottom = 0;
-    this.element.appendChild(newTopEbox);
-    this.element.appendChild(newBotEbox);
+    this.TopEbox = document.createElement("div");
+    this.BotEbox = document.createElement("div");
+    this.TopEbox.classList.add("nodeEbox");
+    this.BotEbox.classList.add("nodeEbox");
+    this.TopEbox.addEventListener("mousedown", (e) => { e.stopPropagation(); e.preventDefault(); this.handleEboxClick("top") })
+    this.BotEbox.addEventListener("mousedown", (e) => { e.stopPropagation(); e.preventDefault(); this.handleEboxClick("bot") })
+    this.TopEbox.addEventListener("mouseup", (e) => { this.checkNewEdge(e,"top") });
+    this.BotEbox.addEventListener("mouseup", (e) => { this.checkNewEdge(e,"bot") });
+    this.TopEbox.style.top = 0;
+    this.BotEbox.style.bottom = 0;
+    this.element.appendChild(this.TopEbox);
+    this.element.appendChild(this.BotEbox);
   }
   handleClick = e => {
     document.addEventListener("mousemove", this.setPositionFromEvent);
@@ -76,6 +117,7 @@ class Node {
   }
   setPositionFromEvent = e => {
     this.setPosition(e.clientX, e.clientY);
+    nodeBox.checkNodeEdges(this.id);
   }
   moveNodePosition(dx, dy) {
     this.setPosition(this.x + dx, this.y + dy)
@@ -91,8 +133,12 @@ class Node {
       if (nodeBox.originEbox.type != type) {
         const [lowNodeId,highNodeId] = type=="top"? [this.id,nodeBox.originEbox.NodeId] : [nodeBox.originEbox.NodeId,this.id] ;
         console.log(`${lowNodeId}=>${highNodeId}`);
+        nodeBox.addEdge(lowNodeId,highNodeId);
       }
     }
+  }
+  getEboxPosition(type){
+    return (type=='high')? [this.x-this.width/2+8,this.y-this.height/2] : [this.x-this.width/2+3,this.y+this.height/2];
   }
 }
 
@@ -100,6 +146,7 @@ class NodeBox {
   constructor() {
     this.element = document.getElementById("nodeBox");
     this.nodes = [];
+    this.edges = [];
     this.panOriginX = 0;
     this.panOriginY = 0;
     this.element.addEventListener("mousedown", this.handleClick);
@@ -118,6 +165,7 @@ class NodeBox {
     this.nodes.forEach((node) => { node.moveNodePosition(e.clientX - this.panOriginX, e.clientY - this.panOriginY) })
     this.panOriginX = e.clientX;
     this.panOriginY = e.clientY;
+    this.refreshAllEdges();
   }
   removeNodeById(id) {
     this.nodes.forEach((node) => { if (node.id == id) { node.element.remove() } });
@@ -125,6 +173,25 @@ class NodeBox {
   }
   addNode(e, x, y) {
     nodeBox.nodes.push(new Node(x, y, this.nodes.length == 0 ? 0 : 1 + Math.max(...this.nodes.map((node) => node.id))))
+  }
+  getNodeById = (nid) => this.nodes.filter((node)=>node.id==nid)[0];
+  addEdge(lid,hid){
+    const p1 = this.getNodeById(lid).getEboxPosition('high');
+    const p2 = this.getNodeById(hid).getEboxPosition('low');
+    const newEdge = new Edge(p1,p2);
+    this.element.appendChild(newEdge.element);
+    this.edges.push({lid:lid,hid:hid,edge:newEdge});
+  }
+  checkNodeEdges(nid){
+    this.edges.filter((edge)=>(edge.lid==nid || edge.hid==nid)).forEach((edge)=>this.refreshEdge(edge))
+  }
+  refreshEdge(edge){
+    const p1 = this.getNodeById(edge.lid).getEboxPosition('high');
+    const p2 = this.getNodeById(edge.hid).getEboxPosition('low');
+    edge.edge.rebuildCurve(p1,p2);
+  }
+  refreshAllEdges(){
+    this.edges.forEach((edge)=>this.refreshEdge(edge));
   }
 }
 
@@ -146,3 +213,13 @@ if (document.addEventListener) {
   });
 }
 
+var origin = null;
+// document.addEventListener("mousedown",handleClick)
+function handleClick(e){
+  origin = [e.clientX,e.clientY]
+  document.addEventListener("mousemove",handleEdgeMove);
+  document.addEventListener("mouseup",()=>{document.removeEventListener("mousemove",handleEdgeMove)},{once:true})
+}
+handleEdgeMove = e => {
+  newEdge.rebuildCurve(origin,[e.clientX,e.clientY])
+}
